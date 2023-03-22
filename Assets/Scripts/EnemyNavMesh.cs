6228e768aef168d29dfb45dbea5a94b2f388fa46
dbarkowsky <dylanbarkowsky@gmail.com>
2023-03-22 11:54:32 -0700

    added enemy states for idle, chase, attack

--- Assets/Scripts/EnemyNavMesh.cs
+++ Assets/Scripts/EnemyNavMesh.cs
@@ -5,17 +5,80 @@ using UnityEngine.AI;
 
 public class EnemyNavMesh : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent agent;
-    [SerializeField] private Transform target;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private NavMeshAgent agent;    // for moving on the NavMesh
+    [SerializeField] private Transform target;      // the target to follow
+
+    private float distanceToTarget = float.MaxValue;    // distance to target - default to far away
+    private float chaseRange = 10f;                     // when target is closer than this, chase!
+    private float attackRange = 2f;                     // when target is closer than this, attack!
+
+    private enum EnemyState { IDLE, CHASE, ATTACK };
+    private EnemyState state;
+
+    private void SetState(EnemyState newState)
     {
-        
+        state = newState;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        SetState(EnemyState.IDLE);      // start off in the IDLE state
+    }
+
     void Update()
     {
-        agent.SetDestination(target.transform.position);
+        distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+        // what happens here depends on the state we're currently in!
+        switch (state)
+        {
+            case EnemyState.IDLE: Update_Idle(); break;
+            case EnemyState.CHASE: Update_Chase(); break;
+            case EnemyState.ATTACK: Update_Attack(); break;
+            default: Debug.Log("Invalid state!"); break;
+        }
+    }
+
+    void Update_Idle()
+    {
+        agent.isStopped = true;                             // stop the agent (following)
+        if (distanceToTarget < chaseRange)
+        {
+            SetState(EnemyState.CHASE);
+        }
+    }
+
+    void Update_Chase()
+    {
+        agent.isStopped = false;                            // start the agent (following)
+        agent.SetDestination(target.transform.position);    // follow the target
+        if (distanceToTarget > chaseRange)
+        {
+            SetState(EnemyState.IDLE);
+        }
+        if (distanceToTarget < attackRange)
+        {
+            SetState(EnemyState.ATTACK);
+        }
+    }
+
+    void Update_Attack()
+    {
+        agent.isStopped = false;
+        Debug.Log("Hi-ya!");
+        if (distanceToTarget > attackRange)
+        {
+            SetState(EnemyState.CHASE);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Chase sphere
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+
+        // Attack sphere
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
